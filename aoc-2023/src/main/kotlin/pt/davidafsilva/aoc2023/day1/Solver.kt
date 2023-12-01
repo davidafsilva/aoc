c7159aff3a8f5fd1ebd153c117303ec543477645
David Silva <david@davidafsilva.pt>
2023-12-01 14:35:14 +0000

    day1 - part2

--- aoc-2023/src/main/kotlin/pt/davidafsilva/aoc2023/day1/Solver.kt
+++ aoc-2023/src/main/kotlin/pt/davidafsilva/aoc2023/day1/Solver.kt
@@ -2,9 +2,38 @@ package pt.davidafsilva.aoc2023.day1
 
 import pt.davidafsilva.aoc2022.loadInput
 
+private val orderedDigitsStr = listOf(
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+)
+
 fun main() {
-    val calibrationValues = loadInput(1)
-        .map { line -> line.filter(Char::isDigit).map(Char::digitToInt) }
+    println("1st part: ${loadInput(1).calibrationValues().sum()}")
+    println("2nd part: ${loadInput(1).map(String::normaliseDigits).calibrationValues().sum()}")
+}
+
+private fun Sequence<String>.calibrationValues() =
+    map { line -> line.mapNotNull(Char::digitToIntOrNull) }
         .map { numbers -> numbers.first() * 10 + numbers.last() }
-    println("1st part: ${calibrationValues.sum()}")
+
+private fun String.normaliseDigits(): String {
+    var firstDigitIdx = Int.MAX_VALUE
+    var firstDigit = -1
+    for ((v, d) in orderedDigitsStr.withIndex()) {
+        val idx = indexOf(d)
+        if (idx < 0) continue
+
+        if (idx < firstDigitIdx) {
+            firstDigitIdx = idx
+            firstDigit = v + 1
+        }
+
+        if (idx <= 2) break
+    }
+
+    if (firstDigit == -1) return this
+    return replaceRange(
+        startIndex = firstDigitIdx,
+        endIndex = firstDigitIdx + orderedDigitsStr[firstDigit - 1].length - 1,
+        replacement = firstDigit.toString(),
+    ).normaliseDigits()
 }
