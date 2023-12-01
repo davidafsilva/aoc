30ef64afe2579ec26325772f0e33d70dfabf3674
David Silva <david@davidafsilva.pt>
2023-12-01 10:02:54 +0000

    day1 - part1

--- aoc-2023/src/main/kotlin/pt/davidafsilva/aoc2023/day1/Solver.kt
+++ aoc-2023/src/main/kotlin/pt/davidafsilva/aoc2023/day1/Solver.kt
@@ -1,7 +1,10 @@
-package pt.davidafsilva.aoc2022.day1
+package pt.davidafsilva.aoc2023.day1
 
 import pt.davidafsilva.aoc2022.loadInput
 
 fun main() {
-
+    val calibrationValues = loadInput(1)
+        .map { line -> line.filter(Char::isDigit).map(Char::digitToInt) }
+        .map { numbers -> numbers.first() * 10 + numbers.last() }
+    println("1st part: ${calibrationValues.sum()}")
 }
